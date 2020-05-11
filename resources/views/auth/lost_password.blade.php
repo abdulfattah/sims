a2de537ab2b8dafd030a406adb7b5a2474b186ee
Abd Fattah <a.fattah@ymail.com>
2020-05-11 14:50:20 +0800

    update lowercase

--- resources/views/auth/lost_password.blade.php
+++ resources/views/auth/lost_password.blade.php
@@ -34,7 +34,7 @@
                                     </svg>
                                 </span>
                             </div>
-                            <div class="dx-texteditor-with-icon" data-dx="textbox" data-name="email" data-mode="email" data-placeholder="Email" data-value="" data-validate="true"
+                            <div class="dx-texteditor-with-icon" data-dx="textbox" data-name="email" data-case="lowercase" data-mode="email" data-placeholder="Email" data-value="" data-validate="true"
                                 data-validation-type="required,email" data-validation-group="form"></div>
                         </div>
                     </div>
